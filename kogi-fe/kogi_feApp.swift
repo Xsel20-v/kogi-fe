d906c3246d998bb0386425934d6a05b35ac2ab3e
Xsel20-v <axelbenaya77@gmail.com>
2024-05-07 11:04:52 +0700

    Intial Commit

---
+++ kogi-fe/kogi_feApp.swift
@@ -0,0 +1,17 @@
+//
+//  kogi_feApp.swift
+//  kogi-fe
+//
+//  Created by Jonathan Axel Benaya on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct kogi_feApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
