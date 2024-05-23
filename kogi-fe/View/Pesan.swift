8e0a6b566e22a2af083c816df5d3ce59e1a82d34
Azella Gania Mutyara <azellamutyara@gmail.com>
2024-05-23 10:07:39 +0700

    Push ChatRoomView, ContentView, Pesan

--- kogi-fe/View/Pesan.swift
+++ kogi-fe/View/Pesan.swift
@@ -8,11 +8,78 @@
 import SwiftUI
 
 struct Pesan: View {
+    @Binding var path : NavigationPath
+    @Binding var tabSelection: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        NavigationView {
+            List {
+                Button {
+                    path.append("Chat Room")
+                } label: {
+                    MessageView(
+                        imageName: "person.crop.circle.fill",
+                        name: "Jonathan",
+                        message: "Ya, saya tertarik. Gigi saya sangat sakit 😢",
+                        time: "08.13"
+                    )
+                }
+
+//                NavigationLink(destination: Text("Message Detail View")) {
+//                    MessageView(
+//                        imageName: "person.crop.circle.fill",
+//                        name: "Jonathan",
+//                        message: "Ya, saya tertarik. Gigi saya sangat sakit 😢",
+//                        time: "08.13"
+//                    )
+//                }
+//                NavigationLink(destination: Text("Message Detail View")) {
+//                    MessageView(
+//                        imageName: "person.crop.circle.fill",
+//                        name: "Jonathan",
+//                        message: "Ya, saya tertarik. Gigi saya sangat sakit 😢",
+//                        time: "08.13"
+//                    )
+//                }
+            }
+            .navigationBarTitle("Pesan", displayMode: .large)
+            
+//        }
+    }
+}
+
+struct MessageView: View {
+    let imageName: String
+    let name: String
+    let message: String
+    let time: String
+    
+    var body: some View {
+        HStack {
+            Image(systemName: imageName)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(name)
+                        .font(.headline)
+                    Spacer()
+                    Text(time)
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                Text(message)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding(.vertical, 8)
     }
 }
 
 #Preview {
-    Pesan()
+    Pesan(path: .constant(NavigationPath()), tabSelection: .constant(1))
 }
