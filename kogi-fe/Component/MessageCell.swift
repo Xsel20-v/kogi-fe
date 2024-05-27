dbf3f3bc4ba1f49f2734da7f54d31c24f638b844
Xsel20-v <axelbenaya77@gmail.com>
2024-05-27 12:01:47 +0700

    Bikin ulang message cell karna yg tadi ilang

--- kogi-fe/Component/MessageCell.swift
+++ kogi-fe/Component/MessageCell.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct MessageCell: View {
+    let message: String
+    let timeStamp: String
+    let isMyMessage: Bool
+    
+    var formattedTimeStamp: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        if let date = formatter.date(from: timeStamp) {
+            let timeFormatter = DateFormatter()
+            timeFormatter.dateFormat = "h:mm a"
+            return timeFormatter.string(from: date)
+        }
+        return "Invalid time"
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: isMyMessage ? .trailing : .leading) {
+            Text(message)
+                .padding(10)
+                .foregroundColor(.black)
+                .background(isMyMessage ? Constant.Colors.myMessage : Constant.Colors.notMyMessage)
+                .cornerRadius(10)
+                .shadow(radius: 2)
+                .overlay(alignment: isMyMessage ? .bottomLeading : .bottomTrailing) {
+                    Triangle()
+                        .frame(width: 15, height: 15)
+                        .foregroundColor(isMyMessage ? Constant.Colors.myMessage : Constant.Colors.notMyMessage)
+                        .offset(x: isMyMessage ? -7 : 7, y: 1.5)
+                }
+            
+            Text(formattedTimeStamp)
+                .font(.caption)
+                .foregroundColor(.gray)
+        }
+        .padding()
+            
+        
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        // Start from the top-center
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        // Add line to the bottom-left
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        // Add line to the bottom-right
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        // Close the path
+        path.closeSubpath()
+        return path
     }
 }
 
 #Preview {
-    MessageCell()
+    MessageCell(message: "Hello my name is Jonathan Axel Benaya, i live in Bandung. I Like to code and now I am currently finishing my thesis", timeStamp: "2024-05-23T14:52:29", isMyMessage: false)
 }
